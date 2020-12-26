b1b779fb34f4a019b77752e4a42c32a81d2733d9
Joshua Durai <joshua.durai@sap.com>
2020-12-25 16:56:55 -0800

    Liquor tile layout change

--- frontend/src/app/components/liquor/liquor-tile/liquor-tile.component.ts
+++ frontend/src/app/components/liquor/liquor-tile/liquor-tile.component.ts
@@ -13,7 +13,6 @@ declare var VanillaTilt: any;
 })
 export class LiquorTileComponent implements OnInit {
 
-
   @Input() public liquorItem: ILiquor;
 
 
@@ -26,6 +25,76 @@ export class LiquorTileComponent implements OnInit {
     //   speed: 400
     // });
     // VanillaTilt.init(document.querySelectorAll('.box'));
+
+  }
+
+
+  addToCart(item) {
+    console.log(item);
+  }
+
+
+  generateStyle(category): object {
+    switch (category) {
+      case 'whiskey':
+        return {
+          background: `url('../../../../assets/test/whiskey.jpeg') no-repeat`,
+          'background-size': '122%',
+          'background-position-y': '0%',
+          'background-position-x': '41%'
+        };
+
+      case 'rum':
+        return {
+          background: `url('../../../../assets/test/rum.jpeg') no-repeat`,
+          'background-size': '112%',
+          'background-position-y': '0%',
+          'background-position-x': '42%'
+        };
+
+      case 'beer':
+        return {
+          background: `url('../../../../assets/test/beer.jpeg') no-repeat`,
+          'background-size': '136%',
+          'background-position-y': '49%',
+          'background-position-x': '46%'
+        };
+
+      case 'wine':
+        return {
+          background: `url('../../../../assets/test/wine.jpeg') no-repeat`,
+          'background-size': '100%',
+          'background-position-y': '30%',
+          'background-position-x': '0%'
+        };
+
+      case 'vodka':
+        return {
+          background: `url('../../../../assets/test/vodka.jpeg') no-repeat`,
+          'background-size': '104%',
+          'background-position-y': '9%',
+          'background-position-x': '0%'
+        };
+      case 'tequila':
+        return {
+          background: `url('../../../../assets/test/tequila.jpeg') no-repeat`,
+          'background-size': '104%',
+          'background-position-y': '9%',
+          'background-position-x': '0%'
+        };
+
+      case 'gin':
+        return {
+          background: `url('../../../../assets/test/gin.jpeg') no-repeat`,
+          'background-size': '104%',
+          'background-position-y': '9%',
+          'background-position-x': '0%'
+        };
+    }
+
+
+
+
 
   }
 
