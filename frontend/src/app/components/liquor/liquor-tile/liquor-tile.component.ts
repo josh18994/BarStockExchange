1e4ed4070ed1d328ddf2914f0bc77573a1ba0ae0
Joshua Durai <joshua.durai@sap.com>
2021-01-10 23:01:48 -0800

    Bug Fix - height and total pin

--- frontend/src/app/components/liquor/liquor-tile/liquor-tile.component.ts
+++ frontend/src/app/components/liquor/liquor-tile/liquor-tile.component.ts
@@ -41,73 +41,12 @@ export class LiquorTileComponent implements OnInit {
 
 
   updateCart(item: ILiquor) {
-    const quantity = this.cart.filter(x => x.liquorId === item._id)[0]?.quantity;
+    const quantity = this.cart.filter(x => x.liquor._id === item._id)[0]?.quantity;
     this.store.dispatch(new UpdateCart(item._id, !!quantity ? quantity + 1 : 1));
   }
 
   getQuantity(id: string) {
-    return this.cart.filter(x => x.liquorId === id)[0]?.quantity || '';
-  }
-
-
-  generateStyle(category): object {
-    switch (category) {
-      case 'whiskey':
-        return {
-          background: `url('../../../../assets/test/whiskey.jpeg') no-repeat`,
-          'background-size': '122%',
-          'background-position-y': '0%',
-          'background-position-x': '41%'
-        };
-
-      case 'rum':
-        return {
-          background: `url('../../../../assets/test/rum.jpeg') no-repeat`,
-          'background-size': '112%',
-          'background-position-y': '0%',
-          'background-position-x': '42%'
-        };
-
-      case 'beer':
-        return {
-          background: `url('../../../../assets/test/beer.jpeg') no-repeat`,
-          'background-size': '136%',
-          'background-position-y': '49%',
-          'background-position-x': '46%'
-        };
-
-      case 'wine':
-        return {
-          background: `url('../../../../assets/test/wine.jpeg') no-repeat`,
-          'background-size': '100%',
-          'background-position-y': '30%',
-          'background-position-x': '0%'
-        };
-
-      case 'vodka':
-        return {
-          background: `url('../../../../assets/test/vodka.jpeg') no-repeat`,
-          'background-size': '104%',
-          'background-position-y': '9%',
-          'background-position-x': '0%'
-        };
-      case 'tequila':
-        return {
-          background: `url('../../../../assets/test/tequila.jpeg') no-repeat`,
-          'background-size': '104%',
-          'background-position-y': '9%',
-          'background-position-x': '0%'
-        };
-
-      case 'gin':
-        return {
-          background: `url('../../../../assets/test/gin.jpeg') no-repeat`,
-          'background-size': '104%',
-          'background-position-y': '9%',
-          'background-position-x': '0%'
-        };
-    }
-
+    return this.cart.filter(x => x.liquor._id === id)[0]?.quantity || '';
   }
 }
 
