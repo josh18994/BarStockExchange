2468c453bb44701eca8a98092e0d5dbdebb71f63
Joshua Durai <josh18994@gmail.com>
2021-02-13 16:35:54 -0800

    prod env var ws -> wss

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiURL: 'https://api.barstockex.com/graphql',
-  apiWsUrl: 'ws://api.barstockex.com/graphql'
+  apiWsUrl: 'wss://api.barstockex.com/graphql'
 };
